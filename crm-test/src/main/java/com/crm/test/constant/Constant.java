c64be1cba2cbdf5c1aed4b041060030f5e9a15ab
xiuya <hexiuya0902@hotmail.com>
2018-02-28 23:51:15 +0800

    remove crm-test/

--- crm-test/src/main/java/com/crm/test/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.crm.test.constant;
-
-public class Constant {
-	public static final String COMMA = ",";
-	
-	public static final String DOUBLE_VERTICAL_LINE = "||";
-	
-	public static final String NEWLINE = "\n";
-}
